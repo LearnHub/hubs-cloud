a8be7d325d4b17cfe80d25de0434fb5e3b8be286
Rupert Rawnsley <rupert.rawnsley@gmail.com>
2025-06-03 13:23:01 +0100

    Switch to hubs.localhost to simplify testing setup

--- community-edition/services/dockerbuildall.sh
+++ community-edition/services/dockerbuildall.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
+# Exit if any command fails
 set -e
 
-docker_username="avncloud"
+# Use the same directory as the script
+cd "$(dirname "$0")"
 
-########################
-tagPrefix=""
-if ! [ -z $docker_username ]; then
-    echo "docker login [$docker_username]"
-    docker login --username $docker_username
-    tagPrefix=$docker_username/
+# Expect either 'build' or 'push' as first parameter
+build_or_push=$1
+if [[ "$build_or_push" =~ ^(build|push)$ ]]; then
+    echo "Action '$build_or_push'"
+else
+    echo "ERROR: Instruction parameter '$build_or_push' is not one of { build, push }"
+    exit 1
 fi
 
-images=""
+# Build tag expected as second parameter
+docker_tag=$2
+if [[ "$docker_tag" =~ ^(local|alpha|prod)$ ]]; then
+    if [ $build_or_push == "build" ]; then
+        echo "Building with docker tag '$docker_tag'"
+    else
+        if [ $docker_tag != "local" ]; then
+            echo "Pushing with docker tag '$docker_tag'"
+        else
+            echo "ERROR: Pushing tag 'local' doesn't make sense as images only expected to be used locally"
+            exit 1
+        fi
+    fi
+else
+    echo "ERROR: Docker tag '$docker_tag' is not one of { local, alpha, prod }"
+    exit 1
+fi
+
+# Record images that are processed for final report
+images_processed=""
 for dir in */ ; do
     if [ -d "$dir" ]; then
+        # Remove trailing slash
         dir="${dir%/}"
-        tag_name=$tagPrefix"${dir}:alpha"
-        msg="Building Docker image (-t $tag_name) for $dir"
-        msg_len=${#msg}
-        pad=$(printf '#%.0s' $(seq 1 $((msg_len + 8))))
-
-        echo $pad; echo "### $msg ###"; echo $pad
-
-        docker build -t "$tag_name" -f ./$dir/Dockerfile ./$dir
-        if ! [ -z $tagPrefix ]; then
+        tag_name="avncloud/${dir}:${docker_tag}"
+        if [ $build_or_push == "build" ]; then
+            echo "Building ${tag_name}..."
+            docker build --platform=linux/arm64 -t "$tag_name" -f ./$dir/Dockerfile ./$dir
+        else
+            echo "Pushing ${tag_name}..."
             docker push $tag_name
         fi
-        images=$images'\n'$tag_name
+        images_processed=$images_processed'\n'$tag_name
     fi
 done
 
-printf done:$images
+printf "\nProcessed:${images_processed}\n"
